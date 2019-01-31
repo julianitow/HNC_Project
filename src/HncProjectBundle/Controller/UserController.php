8612b1f6fa2d6a3e91785aca419b99f9d0d729a7
julien-guillan <guillan.julien@live.com>
2019-01-31 18:45:56 +0000

    User entity added and started to work on login form

--- src/HncProjectBundle/Controller/UserController.php
+++ src/HncProjectBundle/Controller/UserController.php
@@ -2,22 +2,48 @@
 
 namespace HncProjectBundle\Controller;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use HncProjectBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 
 class UserController extends Controller
 {
-    public function registerAction()
+    public function registerAction(Request $request)
     {
-        return $this->render('HncProjectBundle:User:register.html.twig', array(
-            // ...
-        ));
+        $user = new User();
+        $registerFormBuider = $this->get('form.factory')->createBuilder(FormType::class, $user, ['allow_extra_fields' => true]);
+        $registerFormBuider
+            ->add('firstname', TextType::class, ['label' => "Firstname :", 'attr' => ['class' => "form-control", 'placeholder' => "First Name"]])
+            ->add('lastname', TextType::class, ['label' => "Lastname", 'attr' => ['class' => "form-control", 'placeholder => Last Name']])
+            ->add('email', EmailType::class)
+            ->add('birthday', BirthdayType::class)
+            ->add('phonenumber', TelType::class)
+            ->add('receiveMarketing', CheckboxType::class)
+            ->add('password', RepeatedType::class)
+            ->add('Register', SubmitType::class)
+            ;
+        $registerForm = $registerFormBuider->getForm();
+        $registerForm->handleRequest($request);
+
+
+        return $this->render('@HncProject\User\register.html.twig', ['registerForm' => $registerForm->createView()]);
     }
 
     public function loginAction()
     {
-        return $this->render('HncProjectBundle:User:login.html.twig', array(
-            // ...
-        ));
+        return $this->render('@HncProject\User\login.html.twig');
     }
 
+    public function logoutAction()
+    {
+        return $this->render('@HncProject\User\logout.html.twig');
+    }
 }
---
+++ src/HncProjectBundle/Entity/User.php
@@ -0,0 +1,283 @@
+<?php
+
+namespace HncProjectBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="HncProjectBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", length=100)
+     */
+    private $firstname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastname", type="string", length=100)
+     */
+    private $lastname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="birthday", type="datetime")
+     */
+    private $birthday;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phonenumber", type="string", length=15, unique=true)
+     */
+    private $phonenumber;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="receiveMarketing", type="boolean")
+     */
+    private $receiveMarketing;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="portfolioID", type="integer", unique=true)
+     */
+    private $portfolioID;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     *
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set birthday
+     *
+     * @param \DateTime $birthday
+     *
+     * @return User
+     */
+    public function setBirthday($birthday)
+    {
+        $this->birthday = $birthday;
+
+        return $this;
+    }
+
+    /**
+     * Get birthday
+     *
+     * @return \DateTime
+     */
+    public function getBirthday()
+    {
+        return $this->birthday;
+    }
+
+    /**
+     * Set phonenumber
+     *
+     * @param string $phonenumber
+     *
+     * @return User
+     */
+    public function setPhonenumber($phonenumber)
+    {
+        $this->phonenumber = $phonenumber;
+
+        return $this;
+    }
+
+    /**
+     * Get phonenumber
+     *
+     * @return string
+     */
+    public function getPhonenumber()
+    {
+        return $this->phonenumber;
+    }
+
+    /**
+     * Set receiveMarketing
+     *
+     * @param boolean $receiveMarketing
+     *
+     * @return User
+     */
+    public function setReceiveMarketing($receiveMarketing)
+    {
+        $this->receiveMarketing = $receiveMarketing;
+
+        return $this;
+    }
+
+    /**
+     * Get receiveMarketing
+     *
+     * @return bool
+     */
+    public function getReceiveMarketing()
+    {
+        return $this->receiveMarketing;
+    }
+
+    /**
+     * Set portfolioID
+     *
+     * @param integer $portfolioID
+     *
+     * @return User
+     */
+    public function setPortfolioID($portfolioID)
+    {
+        $this->portfolioID = $portfolioID;
+
+        return $this;
+    }
+
+    /**
+     * Get portfolioID
+     *
+     * @return int
+     */
+    public function getPortfolioID()
+    {
+        return $this->portfolioID;
+    }
+}
+
