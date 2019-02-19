6d1e3160a24e68343f7cc9df4119fe61721eb79f
IanHolderness <47216246+IanHolderness@users.noreply.github.com>
2019-02-19 20:07:12 +0000

    Updated Index file

--- Index.php
+++ Index.php
@@ -12,6 +12,8 @@ $currentuser=1;
     <!- ************ Stylesheets ******* ->
     <link href="ASSETS/CSS/Style.css" rel="stylesheet" type="text/css" />
     <link href="ASSETS/CSS/MENU.css" rel="stylesheet" type="text/css" />
+    <link href="ASSETS/CSS/Shares.css" rel="stylesheet" type="text/css" />
+    <link href="ASSETS/CSS/News.css" rel="stylesheet" type="text/css" />
     <!- ******************************** ->
     <!- This will change depending on user settings, but by default
         will just say the name of the site ->
@@ -39,15 +41,44 @@ $currentuser=1;
               <a href="">Test3</a>
             <?php } ?>
               <a href="admin.php">Administration</a>
-
+              <label for='Currency'>Select Currency :</label>
+              <select id="Currency">
+                  <option value="GBP" selected="selected">Pounds (GBP)</option>
+                  <option value="EUR">Euro (EUR)</option>
+                  <option value="USD">US Dollar (USD)</option>
+                  <option value="ISK">Króna (ISK)</option>
+                  <option value="MXN">Mexican Peso (MXN)</option>
+                  <option value="CHF">Swiss Franc (CHF)</option>
+                  <option value="AUD">Australian Dollar (AUD)</option>
+                  <option value="CNY">Chinese Yuan (CNY)</option>
+                  <option value="SEK">Swedish Krona (SEK)</option>
+                </select>
               <a href="php/logout.php">Log Out</a>
-              <a href="">Settings &#9881</a>
+              <a href="">&#9881; Settings</a>
               <a href="contactus.php">Contact Us</a>
       </nav>
-      <section id='FsteScroll'>
 
+      <section id='FsteScroll'>
+        <aside id='Updated'><aside/>
       </section>
+      <section id="NewsArticles"></section>
+
   </body>
   <script src="JS/Menu.js"></script>
-
+  <script src="JS/functions.js"></script>
+  <script src="JS/PrepShare.js"></script>
+  <script src="JS/News.js"></script>
+  <script src="JS/money.js"></script> <!- https://openexchangerates.github.io/money.js -!>
+  <script>
+	document.onreadystatechange = function(){
+		if(document.readyState=="complete") {
+			prepareMenu(); // Attaches event listener to burger btn
+      mkShareData(); // Makes the fste share elements
+      updateShareData(); // Sets the interval to update the share data
+      UpdateCurrency(); // Adds an evrnt listener to the currency dropdown
+      LoadNewsStories(); // Creates the first run of news articles
+      updateNews(); // Sets interval for getting news articles
+		}
+	}
+	</script>
 </html>
