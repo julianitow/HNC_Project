a0ce454fa0d3a8bb8e3a56f4380fc12bb8c497e1
julien-guillan <guillan.julien@live.com>
2019-03-06 13:11:59 +0100

    Bootstrap template added to be more effective

--- src/HncProjectBundle/Controller/DefaultController.php
+++ src/HncProjectBundle/Controller/DefaultController.php
@@ -9,19 +9,22 @@ class DefaultController extends Controller
 {
     public function indexAction(Request $request)
     {
+        $logged_in = false;
         if ( null != $request->getSession()->get('token'))
         {
             //var_dump($request->getSession()->get('token')->getUser());
             //var_dump($request->getSession()->get('_security_main'));
             $token = $token = $this->get('session')->get('token');
             $this->get('security.token_storage')->setToken($token);
+            $logged_in = true;
         }
 
         $news = $this->get_JSON("https://newsapi.org/v2/top-headlines?language=en&country=gb&category=business&apiKey=ba3059047e3548fab44689b0b0870d93");
         $shares = null; //$this->share_json();
         $ftse_data = $this->ftse_json();
 
-        return $this->render('@HncProject/Default/index.html.twig', ['articles'=> $news->articles, 'shares' => $shares, 'ftse_data' => $ftse_data]);
+        return $this->render('@HncProject/Default/index.html.twig', ['logged_in' => $logged_in,
+            'articles'=> $news->articles, 'shares' => $shares, 'ftse_data' => $ftse_data]);
     }
 
     public function get_JSON($url)
